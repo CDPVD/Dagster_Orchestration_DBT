b4a19ee65f10e4016fd438e0df8f5d673a42134f
sadqim <sadqim@csvdc.qc.ca>
2026-06-10 17:24:35 -0400

    Chore: configure daily schedules for prod

--- Dagster_Orchestration_DBT/schedules.py
+++ Dagster_Orchestration_DBT/schedules.py
@@ -10,7 +10,7 @@ schedules = [
     build_schedule_from_dbt_selection(
          [CDPVD_dbt_assets],
          job_name="CDPVD_DAILY",
-         cron_schedule="15 5 * * *",
+         cron_schedule="15 8 * * *",
          dbt_select="fqn:*",
      ),
 
